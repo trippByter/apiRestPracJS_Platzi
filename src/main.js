b7bb938ad6c4be61d46ccba40ae3d3907397d6f2
Preent Loo <psilocybit5211@gmail.com>
2022-06-09 22:25:42 -0400

    Lista de categorías.

--- src/main.js
+++ src/main.js
@@ -36,3 +36,47 @@ async function getTrendingMoviesPreview() {
     });
 };
 getTrendingMoviesPreview();
+
+
+// Movie's categories
+async function getCategoriesPreview() {
+    const res = await fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=" + API_KEY);
+    const data = await res.json();
+    // Review the resonse in the API docs
+    const categories = data.genres;
+    console.log(data);
+    
+    // Obtain the movie image from the API
+    // and construct a HTML element.
+    categories.forEach(category => {
+        // CREATE  <div/>  INSIDE  <categoriesPreview-list/>
+        // <article class="categoriesPreview-list">
+        //   <div class="category-container">
+        //    <h3 id="id14" class="category-title">Acción</h3>
+        //   </div>
+        // </article>
+        
+        // 1th Get <article class="categoriesPreview-list">
+        const previewCategoriesContainer = document.querySelector("#categoriesPreview .categoriesPreview-list");
+        
+        // 2nd Create <div class="category-container">
+        const categoryContainer = document.createElement("div");
+        categoryContainer.classList.add("category-container");
+        
+        // 3rd Create <h3 id="id+category.id" class="category-title"> 
+        const categoryTitle = document.createElement("h3");
+        categoryTitle.classList.add("category-title");
+        categoryTitle.setAttribute("id", "id" + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        // 4th Add <h3 id="id+category.id" class="category-title">
+        // To <div class="category-container">
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+    });
+};
+getCategoriesPreview();
+/*
+<class="" id="Adventure">Adventure</class=>
+*/
